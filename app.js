83f945ac1a66f6547348cfe5e7422d2dfe716d2e
Jadatu13 <jadatu13@outlook.com>
2022-03-31 10:42:23 +1300

    Finished MERN A to Z Worksheet
    Finished tutorial & deployed to Heroku.

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const books = require("./routes/api/books");
+const app = express();
+
+connectDB();
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json({ extended: false }));
+app.use("/api/books", books);
+app.use(express.static("build"));
+
+const path = require("path");
+
+app.use(
+  express.static(
+    path.resolve(__dirname, "./cise_ass1a_worksheet3_frontend/build")
+  )
+);
+app.get("*", function (request, response) {
+  response.sendFile(
+    path.resolve(
+      __dirname,
+      "./cise_ass1a_worksheet3_frontend/build",
+      "index.html"
+    )
+  );
+});
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
